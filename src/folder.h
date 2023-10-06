ebf11299b69ceb275d44fab82f092ab512b128de
naitea9527 <a22057916w@gmail.com>
2023-10-06 19:50:19 +0800

    test folder->path(), folder->name()

--- src/folder.h
+++ src/folder.h
@@ -1,13 +1,29 @@
 #if !defined(FOLDER)
 #define FOLDER
 
+#include "./node.h"
+
 #include <string>
 
 using namespace std;
 
 class Folder: public Node {
 public:
-    Folder(string path);
+    Folder(string path): _path(path) {};
+
+    string name() const override { return getFilename(); }
+
+    string path() const override { return _path; }
+
+private:
+    string _path;
+
+    string getFilename() const {
+        size_t pos = _path.find_last_of("/\\");  // find the last separator either '/' or '\'
+        if(pos != string::npos) // Extract the filename
+            return _path.substr(pos + 1);
+        return _path;    // If no separator is found, return path as name
+    }
 };
 
 
--- test/ut_folder.h
+++ test/ut_folder.h
@@ -6,3 +6,15 @@
 TEST(FolderSuite, Ping) {
     ASSERT_TRUE(true);
 }
+
+TEST(FolderSuite, GetFileName) {
+    Node * folder = new Folder("test/dir1");
+    EXPECT_TRUE(folder->name() == "dir1");
+    delete folder;
+}
+
+TEST(FolderSuite, GetFolderPath) {
+    Node * folder = new Folder("test/dir1");
+    EXPECT_TRUE(folder->path() == "test/dir1");
+    delete folder;
+}
