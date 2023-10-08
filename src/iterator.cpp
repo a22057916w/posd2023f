d21ecf882d53767b0e44b7fee315c1cc53e798f2
naitea9527 <a22057916w@gmail.com>
2023-10-08 13:44:50 +0800

    imp FolderIterator::fisrt(), and isDone()

--- src/iterator.cpp
+++ src/iterator.cpp
@@ -6,3 +6,12 @@
 
 FolderIterator::FolderIterator(Folder * composite): _composite(composite) {}
 
+void FolderIterator::first() {
+    list<Node *> _components = _composite->getComponents();
+    _it = _components.begin();
+}
+
+bool FolderIterator::isDone() const {
+    list<Node *> _components = _composite->getComponents();
+    return _it == _components.end();
+}
