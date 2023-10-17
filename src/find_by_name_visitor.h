92f495aa4e38255fa66c710397b68473157c39e0
naitea9527 <a22057916w@gmail.com>
2023-10-18 02:58:32 +0800

    imping visitFolder()

--- src/find_by_name_visitor.h
+++ src/find_by_name_visitor.h
@@ -13,13 +13,24 @@ class FindByNameVisitor : public Visitor {
 public:
     FindByNameVisitor(string name): _name(name) {};
 
-    void visitFile(File * file) {
-        if (file->name() == _name) {
+    void visitFile(File * file) override {
+        if (file->name() == _name)
             _pathList.push_back(file->path());
-        }
     }
 
-    void visitFolder(Folder * folder);
+    void visitFolder(Folder * folder) {
+        if (folder->name() == _name)
+            _pathList.push_back(folder->path());
+
+        // need to create inner class folderIteartor 
+        auto it = compound->createIterator();
+         for(it->first(); !it->isDone(); it->next()){
+            it->currentItem()->accept(this);
+            // std::list<string> paths = (*it)->findByName(_name);
+            // for (auto i = paths.begin(); i != paths.end(); i++)
+            //     pathList.push_back(*i);   
+        }
+    }
 
     std::list<string> getPaths() const { return _pathList; }
 
