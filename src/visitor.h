877feb0216b612fff0756e6b466d76cbaf0a2a06
naitea9527 <a22057916w@gmail.com>
2023-10-19 02:26:20 +0800

    make visitorFolder() pure virtual

--- src/visitor.h
+++ src/visitor.h
@@ -7,5 +7,5 @@ class Visitor {
 public:
     virtual ~Visitor() {};
     virtual void visitFile(File * file) = 0;
-    void visitFolder(Folder * folder) {};
+    virtual void visitFolder(Folder * folder) = 0;
 };
