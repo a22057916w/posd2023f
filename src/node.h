9682f5698897617f74d3c81968c31e4c59f4f1a4
naitea9527 <a22057916w@gmail.com>
2023-10-18 20:25:30 +0800

    void visitFolder(Folder * folder) {};

--- src/node.h
+++ src/node.h
@@ -66,5 +66,5 @@ public:
         throw string("This node does not support deleting sub node");
     }
 
-    virtual void accept(Visitor * visitor);
+    virtual void accept(Visitor * visitor) {};
 };
