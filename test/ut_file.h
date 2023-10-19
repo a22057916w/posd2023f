dd5a5c0dc224c722082de45fd7a7bee0137f9698
naitea9527 <a22057916w@gmail.com>
2023-10-19 14:51:29 +0800

    TEST: CreateSuccess, CreateFail

--- test/ut_file.h
+++ test/ut_file.h
@@ -1,21 +1,32 @@
-#pragma once 
+#pragma once
 
 #include "../src/file.h"
 #include "../src/find_by_name_visitor.h"
 
-TEST(FileTest, normal) {
-    File hello("/Users/user/home/hello.txt");
-    ASSERT_EQ("hello.txt", hello.name());
-    ASSERT_EQ("/Users/user/home/hello.txt", hello.path());
+#include <string>
+
+using std::string;
+
+// const string _cwd = "/Users/willy/Documents/code/posd2023f";
+#define CWD "/Users/willy/Documents/code/posd2023f"
+
+TEST(FileTest, CreateSuccess)
+{
+    File * hello = new File(CWD "/Users/user/home/hello.txt");
+    ASSERT_EQ(hello->path(), "/Users/willy/Documents/code/posd2023f/Users/user/home/hello.txt");
+}
+
+TEST(FileTest, CreateFail) {
+    EXPECT_THROW(File random(CWD "/User/random"), string);
 }
 
 TEST(FileTest, VisitFindByName) {
-    File * hello = new File("/Users/user/home/hello.txt");
+    File * hello = new File(CWD "/Users/user/home/hello.txt");
     FindByNameVisitor * visitor = new FindByNameVisitor("hello.txt");
-    
+
     hello->accept(visitor);
     EXPECT_EQ(1, visitor->getPaths().size());
-    EXPECT_EQ("/Users/user/home/hello.txt", visitor->getPaths().front());
+    EXPECT_EQ(CWD "/Users/user/home/hello.txt", visitor->getPaths().front());
 
     delete hello;
     delete visitor;
