92ead49e464094921af344d9f16d40f084787c48
naitea9527 <a22057916w@gmail.com>
2023-10-19 15:38:52 +0800

    add CWD to all path

--- test/ut_node.h
+++ test/ut_node.h
@@ -4,30 +4,32 @@
 #include "../src/file.h"
 #include "../src/folder.h"
 
+#define CWD "/Users/willy/Documents/code/posd2023f"
+
 class NodeTest: public ::testing::Test {
 protected:
     void SetUp() {
-        home = new Folder("/Users/user/home");
+        home = new Folder(CWD "/Users/user/home");
 
-        profile = new File("/Users/user/home/my_profile");
+        profile = new File(CWD "/Users/user/home/my_profile");
         home->add(profile);
 
-        download = new Folder("/Users/user/home/Downloads");
+        download = new Folder(CWD "/Users/user/home/Downloads");
         home->add(download);
 
-        document = new Folder("/Users/user/home/Documents");
+        document = new Folder(CWD "/Users/user/home/Documents");
         home->add(document);
 
-        note = new File("/Users/user/home/Documents/note.txt");
+        note = new File(CWD "/Users/user/home/Documents/note.txt");
         document->add(note);
 
-        favorite = new Folder("/Users/user/home/Documents/favorites");
+        favorite = new Folder(CWD "/Users/user/home/Documents/favorites");
         document->add(favorite);
-        ddd = new File("/Users/user/home/Documents/favorites/domain-driven-design.pdf");
+        ddd = new File(CWD "/Users/user/home/Documents/favorites/domain-driven-design.pdf");
         favorite->add(ddd);
-        ca = new File("/Users/user/home/Documents/favorites/clean-architecture.pdf");
+        ca = new File(CWD "/Users/user/home/Documents/favorites/clean-architecture.pdf");
         favorite->add(ca);
-        cqrs = new File("/Users/user/home/Documents/favorites/cqrs.pdf");
+        cqrs = new File(CWD "/Users/user/home/Documents/favorites/cqrs.pdf");
         favorite->add(cqrs);
     }
 
@@ -52,6 +54,8 @@ protected:
     Node * ddd;
     Node * ca;
     Node * cqrs;
+
+    
 };
 
 
@@ -60,33 +64,33 @@ TEST_F(NodeTest, numberOfFiles) {
 }
 
 TEST_F(NodeTest, find_a_file) {
-    Node * result = home->find("/Users/user/home/Documents/favorites/cqrs.pdf");
+    Node * result = home->find(CWD "/Users/user/home/Documents/favorites/cqrs.pdf");
     ASSERT_EQ(cqrs, result);
 }
 
 TEST_F(NodeTest, find_a_file_not_exist) {
-    Node * result = home->find("/Users/user/home/Documents/favorites/helloworld.pdf");
+    Node * result = home->find(CWD "/Users/user/home/Documents/favorites/helloworld.pdf");
     ASSERT_EQ(nullptr, result);
 }
 
 TEST_F(NodeTest, find_a_folder) {
-    Node * result = document->find("/Users/user/home/Documents/favorites");
+    Node * result = document->find(CWD "/Users/user/home/Documents/favorites");
     ASSERT_EQ(favorite, result);
 }
 
 TEST_F(NodeTest, find_a_folder_not_exist) {
-    Node * result = document->find("/Users/user/home/Documents/not_like");
+    Node * result = document->find(CWD "/Users/user/home/Documents/not_like");
     ASSERT_EQ(nullptr, result);
 }
 
 TEST_F(NodeTest, delete_a_file) {
-    ASSERT_EQ(cqrs->name(), home->find("/Users/user/home/Documents/favorites/cqrs.pdf")->name());
-    home->remove("/Users/user/home/Documents/favorites/cqrs.pdf");
-    ASSERT_EQ(nullptr, home->find("/Users/user/home/Documents/favorites/cqrs.pdf"));
+    ASSERT_EQ(cqrs->name(), home->find(CWD "/Users/user/home/Documents/favorites/cqrs.pdf")->name());
+    home->remove(CWD "/Users/user/home/Documents/favorites/cqrs.pdf");
+    ASSERT_EQ(nullptr, home->find(CWD "/Users/user/home/Documents/favorites/cqrs.pdf"));
 }
 
 TEST_F(NodeTest, delete_a_folder) {
-    ASSERT_EQ(favorite->name(), home->find("/Users/user/home/Documents/favorites")->name());
-    home->remove("/Users/user/home/Documents/favorites");
-    ASSERT_EQ(nullptr, home->find("/Users/user/home/Documents/favorites"));
+    ASSERT_EQ(favorite->name(), home->find(CWD "/Users/user/home/Documents/favorites")->name());
+    home->remove(CWD "/Users/user/home/Documents/favorites");
+    ASSERT_EQ(nullptr, home->find(CWD "/Users/user/home/Documents/favorites"));
 }
