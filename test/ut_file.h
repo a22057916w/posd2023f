640acc95c52a9209b49120722aac426a7382ceb4
naitea9527 <a22057916w@gmail.com>
2023-10-19 16:41:29 +0800

    test all file function

--- test/ut_file.h
+++ test/ut_file.h
@@ -9,14 +9,36 @@ using std::string;
 
 #define CWD "/Users/willy/Documents/code/posd2023f"
 
-TEST(FileTest, CreateSuccess)
-{
+TEST(FileTest, CreateFileSuccess) {
     File * hello = new File(CWD "/Users/user/home/hello.txt");
     ASSERT_EQ(hello->path(), "/Users/willy/Documents/code/posd2023f/Users/user/home/hello.txt");
+    delete hello;
 }
 
-TEST(FileTest, CreateFail) {
+TEST(FileTest, CreateFileFail) {
     EXPECT_ANY_THROW(File random(CWD "/User/random"));
+}
+
+TEST(FileTest, createFolderFail) {
+    EXPECT_ANY_THROW(File home(CWD "/Users/user/home"));
+}
+
+TEST(FileTest, FindFileExist) {
+    Node * hello = new File(CWD "/Users/user/home/hello.txt");
+    EXPECT_EQ(hello, hello->find(CWD "/Users/user/home/hello.txt"));
+    delete hello;
+}
+
+TEST(FileTest, FindFileNotExist) {
+    Node * hello = new File(CWD "/Users/user/home/hello.txt");
+    EXPECT_EQ(nullptr, hello->find(CWD "/Users/user/home/note.txt"));
+    delete hello;
+}
+
+TEST(FileTest, NumberOFFiles) {
+    Node * hello = new File(CWD "/Users/user/home/hello.txt");
+    EXPECT_EQ(1, hello->numberOfFiles());
+    delete hello;
 }
 
 TEST(FileTest, VisitFindByName) {
