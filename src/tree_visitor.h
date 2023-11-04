5322944e34b332789e8395528b4f947440141c08
naitea9527 <a22057916w@gmail.com>
2023-11-04 21:01:13 +0800

    deving traversByName()

--- src/tree_visitor.h
+++ src/tree_visitor.h
@@ -2,13 +2,61 @@
 
 #include "visitor.h"
 #include "order_by.h"
+#include "folder.h"
+#include "file.h"
+
+#include <string>
+
+using std::string;
 
 class TreeVisitor: public Visitor {
 public:
-    TreeVisitor(OrderBy orderBy) {};
+    TreeVisitor(OrderBy orderBy): _orderBy(orderBy) {
+        _currLevel = -1;
+        _prevLevel = -1;
+        _tree = "";
+    }
 
-    void visitFile(File * file) {};
-    void visitFolder(Folder * folder) {};
+    void visitFile(File * file) {
+        _tree += file->name();
+    }
 
-    string getTree() { return ""; };
+    void visitFolder(Folder * folder) {
+        if(_orderBy == OrderBy::Name) {
+            _currLevel++;
+            _prevLevel++;
+
+            Iterator * it = folder->createIterator(_orderBy);
+            for(it->first(); !it->isDone(); it->next()) {
+                if(it->next() == it->isDone())
+                    _prevLevel--;
+
+                if(_currLevel > 0) {
+                    _tree += "│";
+                    for(int i = 0; i < _currLevel; i++)
+                        _tree += "|   "; 
+                }
+                _tree += "├── ";
+                if(dynamic_cast<Folder *>(it->currentItem()))
+                    _tree += it->currentItem()->name();
+                    
+                it->currentItem()->accept(this);
+                _tree += "\n";
+            }
+        }
+    }
+
+    void traversByName(Node * folder, string prefix) {
+        Iterator * it = folder->createIterator(_orderBy);
+        for(it->first(); !it->isDone(); it->next()) {
+
+        }
+    }
+
+    string getTree() { return _tree; }
+private:
+    string _tree = "";
+    int _currLevel;
+    int _prevLevel;
+    OrderBy _orderBy;
 };
